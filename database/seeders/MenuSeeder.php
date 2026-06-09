a3fe03691d39f7ed587e22fd1c486027b73458e7
fachrianalbar <fachry.albar29@gmail.com>
2026-06-09 13:23:49 +0700

    seed: remove all seeders except MenuSeeder, rename Workspace to Administrator section

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -14,8 +14,8 @@ class MenuSeeder extends Seeder
             ['name' => 'AI Agents', 'section' => 'Main Menu', 'icon' => 'agents', 'route_name' => 'agents.index', 'active_pattern' => 'agents.*', 'sort_order' => 20],
             ['name' => 'Analytics', 'section' => 'Main Menu', 'icon' => 'analytics', 'route_name' => 'analytics', 'active_pattern' => 'analytics', 'sort_order' => 30],
             ['name' => 'Activity', 'section' => 'Main Menu', 'icon' => 'activity', 'route_name' => 'activity', 'active_pattern' => 'activity', 'sort_order' => 40],
-            ['name' => 'Settings', 'section' => 'Workspace', 'icon' => 'settings', 'route_name' => 'settings', 'active_pattern' => 'settings', 'sort_order' => 10],
-            ['name' => 'Menu Management', 'section' => 'Workspace', 'icon' => 'menu', 'route_name' => 'menus.index', 'active_pattern' => 'menus.*', 'sort_order' => 20],
+            ['name' => 'Settings', 'section' => 'Administrator', 'icon' => 'settings', 'route_name' => 'settings', 'active_pattern' => 'settings', 'sort_order' => 10],
+            ['name' => 'Menu Management', 'section' => 'Administrator', 'icon' => 'menu', 'route_name' => 'menus.index', 'active_pattern' => 'menus.*', 'sort_order' => 20],
         ];
 
         foreach ($menus as $menu) {
